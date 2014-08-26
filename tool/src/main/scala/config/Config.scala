9f6b13fbac3458b2abd9775780387368ce514b6c
tudorraul <tudorraul@gmail.com>
2014-08-26 17:55:30 +0100

    Refactoring config

--- tool/src/main/scala/config/Config.scala
+++ tool/src/main/scala/config/Config.scala
@@ -1,26 +1,29 @@
 package goo
 
-import com.amazonaws.auth.{ AWSCredentialsProviderChain, AWSCredentials }
+import com.amazonaws.auth.{AWSCredentialsProviderChain, AWSCredentials}
 import com.amazonaws.internal.StaticCredentialsProvider
 import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.constructor.Constructor
-import java.util.{ Map => JavaMap }
-import collection.mutable.{ Map => MutableMap }
+import java.util.{Map => JavaMap}
+import collection.mutable.{Map => MutableMap}
 import collection.JavaConversions.mapAsScalaMap
 import scala.reflect.BeanProperty
 import scala.util.control.Exception.allCatch
 
 object Config {
 
-  lazy val cloudformationHome = new java.io.File(System.getProperty("user.dir") + "/../cloudformation").getCanonicalPath()
+  lazy val cloudformationHome = new java.io.File(System.getProperty("user.dir") + "/../cloudformation").getCanonicalPath
 
-  lazy val awsUserCredentials: Option[AWSCredentialsProviderChain] = {
-
-    getFogConfig("default").map( credentials => {
-      val fogCredentials = new FogAWSCredentials(credentials._1, credentials._2)
-      new AWSCredentialsProviderChain(new StaticCredentialsProvider(fogCredentials))
-    })
-  }
+  lazy val awsUserCredentials: Option[AWSCredentialsProviderChain] =
+    getFogConfig("default").flatMap {
+      credentials =>
+        for (
+          accessKey <- credentials.get("aws_access_key_id");
+          secretKey <- credentials.get("aws_secret_access_key")
+        ) yield new FogAWSCredentials(accessKey, secretKey)
+    }.map {
+      x => new AWSCredentialsProviderChain(new StaticCredentialsProvider(x))
+    }
 
   lazy val riffRaffKey: Option[String] = {
 
@@ -39,34 +42,29 @@ object Config {
     }
   }
 
-  private def getFogConfig(group: String): Option[(String, String)] = {
+  private def getFogConfig(group: String): Option[Map[String, String]] = {
     val result = allCatch either scala.io.Source.fromFile(System.getProperty("user.home") + "/.fog")
     val fileConfig = result match {
-      case Right(fogConfig) => {
+      case Right(fogConfig) =>
         val yaml = new Yaml(new Constructor(classOf[JavaMap[String, JavaMap[String, String]]]))
-        val map = yaml.load(fogConfig.mkString).asInstanceOf[JavaMap[String, JavaMap[String,String]]]
+        val map = yaml.load(fogConfig.mkString).asInstanceOf[JavaMap[String, JavaMap[String, String]]]
         fogConfig.close()
         Some(mapAsScalaMap(map))
-      }
-      case Left(e) => {
+      case Left(e) =>
         println(s"Failed to load ~/.fog configuration")
         None
-      }
     }
 
     for {
       config <- fileConfig
       parsedMap <- config.get(group)
-      default: MutableMap[String,String] <- Some(mapAsScalaMap(parsedMap))
-      accessKey <- default.get("aws_access_key_id")
-      secretKey <- default.get("aws_secret_access_key")
-    } yield {
-      (accessKey, secretKey)
-    }
+      default: MutableMap[String, String] <- Some(mapAsScalaMap(parsedMap))
+    } yield default.toMap
   }
 }
 
 class FogAWSCredentials(accessKeyId: String, secretKey: String) extends AWSCredentials {
   def getAWSAccessKeyId: String = accessKeyId
+
   def getAWSSecretKey: String = secretKey
 }
