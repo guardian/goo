9af080fd895747559984af9c967b624436f65b17
tudorraul <tudorraul@gmail.com>
2014-08-29 15:23:05 +0100

    simplify code

--- tool/src/main/scala/commands/RefreshTokensCommand.scala
+++ tool/src/main/scala/commands/RefreshTokensCommand.scala
@@ -1,22 +1,21 @@
 package commands
 
+import commands.refreshAwsTokens.{AwsSts, GOAuthWebServer, Logging}
 import goo.Command
-import commands.refreshAwsTokens.{Logging, GOAuthWebServer, AwsSts}
 
 class RefreshTokensCommand() extends Command {
 
-  import Logging._
+  import commands.refreshAwsTokens.Logging._
 
   override def executeImpl() {
     logger.info("Refreshing AWS tokens")
-    for (
-      gc <- GOAuthWebServer.retrieveCredentials;
+    for {
+      gc <- GOAuthWebServer.retrieveCredentials
       ac <- AwsSts.assumeRole(gc.jsonWebToken, gc.userEmail)
-    ) yield {
+    } yield {
       logger.debug(s"Updating AWS Credentials with $ac")
       logger.info("AWS credentials updated")
       AwsSts.storeCredentials(ac)
     }
   }
-
 }
